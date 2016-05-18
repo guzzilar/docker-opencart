7f9bafdf493d10bf739fe28f34fd79ac88c84276
guzzilar <iam.nuttanon@gmail.com>
2016-05-18 18:01:07 +0700

    Support OpenCart 2.0.1.1 (fixed)

--- src/opencart-2.0.1.1/install.sh
+++ src/opencart-2.0.1.1/install.sh
@@ -9,21 +9,12 @@ rm -R ./_build
 mkdir ./_build
 
 # DEPLOY
-# curl -o opencart-2.0.1.1.zip https://codeload.github.com/opencart/opencart/zip/2.0.1.1
-# unzip -q opencart-2.0.1.1.zip -d ./_build
-# rm opencart-2.0.1.1.zip
+git clone --branch 2.0.1.1 https://github.com/opencart/opencart.git
+mv ./opencart/* ./_build/
+rm -R ./opencart
 
-# TEST
-cp -R ./_resources/opencart/* ./_build/
-# unzip -q ./_resources/opencart-2.0.1.1.zip -d ./_build
-
-# mv ./_build/opencart-2.0.1.1/upload/* ./_build/.
 mv ./_build/upload/config-dist.php ./_build/upload/config.php
 mv ./_build/upload/admin/config-dist.php ./_build/upload/admin/config.php
-
-# Do tricky tasks
-# mv ./_build/install/cli_install.php ./_build/install/cli_install.php.bak
-# cp cli_install.template.php ./_build/install/cli_install.php
 
 # Do install
 cd ./_build/upload/install
@@ -40,14 +31,9 @@ php cli_install.php install --db_driver mysqli \
 
 cd ../../../
 
-# rm -R ./_build/install
-# rm -R ./_build/opencart-2.0.1.1
+rm -R ./_build/upload/install
 rm ../opencart-2.0.1.1-install.sh
 
 chown -R www-data:www-data .
-
-cd ./_build/tests/phpunit
-composer update
-./vendor/bin/phpunit --bootstrap bootstrap.php opencart/admin
 
 apache2 -D FOREGROUND
